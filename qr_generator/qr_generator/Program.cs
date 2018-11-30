824858a11880a8d80634b5528f862d13916486d6
Jerry Wong <man0551hk@yahoo.com.hk>
2018-11-30 13:16:17 +0800

    uopdate

--- qr_generator/qr_generator/Program.cs
+++ qr_generator/qr_generator/Program.cs
@@ -19,7 +19,7 @@ namespace qr_generator
         public static List<string> imageName = new List<string>();
         static void Main(string[] args)
         {
-            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "ticket.png"))
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "ticket_17x7.png"))
             {
                 Console.WriteLine("No ticket.png found!");
             }
@@ -62,10 +62,10 @@ namespace qr_generator
                 //121
                 //Head Table
                 imageName.Add(DrawImage("1", "Oversea", "1", "1"));
-                imageName.Add(DrawImage("12", "Building 1", "1", "1"));
-                imageName.Add(DrawImage("123", "Consulting 1", "1", "1"));
-                imageName.Add(DrawImage("1234", "Infrastructure 1", "1", "1"));
-                imageName.Add(DrawImage("12345", "Business Services", "1", "1"));
+                imageName.Add(DrawImage("12", "Building 1", "12", "12"));
+                imageName.Add(DrawImage("123", "Consulting 1", "123", "123"));
+                imageName.Add(DrawImage("1234", "Infrastructure 1", "1234", "1234"));
+                imageName.Add(DrawImage("12345", "Business Services", "12345", "12345"));
 
 
                 ConvertToPdf();
@@ -79,80 +79,92 @@ namespace qr_generator
         public static string DrawImage(string staffNo, string groupName, string tableNo, string ticketNo)
         {
             Console.WriteLine("Generating images...");
-            try
-            {
-                //creating a image object
-                System.Drawing.Image bitmap = (System.Drawing.Image)Bitmap.FromFile(AppDomain.CurrentDomain.BaseDirectory + "ticket.png"); // set image 
 
-                //draw the image object using a Graphics object
-                Graphics graphicsImage = Graphics.FromImage(bitmap);
-
-                //Set the alignment based on the coordinates   
-                StringFormat stringformat = new StringFormat();
-                stringformat.Alignment = StringAlignment.Far;
-                stringformat.LineAlignment = StringAlignment.Far;
-                StringFormat stringformat2 = new StringFormat();
-                stringformat2.Alignment = StringAlignment.Center;
-                stringformat2.LineAlignment = StringAlignment.Center;
-                //Set the font color/format/size etc..  
-                Color StringColor = System.Drawing.ColorTranslator.FromHtml("#000");//direct color adding
-
-                int x = 0;
-                switch (staffNo.Length)
+                try
                 {
-                    case 1: x = 1763; break;
-                    case 2: x = 1785; break; //+12
-                    case 3: x = 1806; break; //+21  +9
-                    case 4: x = 1836; break; //35 + 9
-                    case 5: x = 1880; break; //35 + 9
+                    //creating a image object
+                    System.Drawing.Image bitmap = (System.Drawing.Image)Bitmap.FromFile(AppDomain.CurrentDomain.BaseDirectory + "ticket_17x7.png"); // set image 
+
+                    //draw the image object using a Graphics object
+                    Graphics graphicsImage = Graphics.FromImage(bitmap);
+
+                    MessagingToolkit.QRCode.Codec.QRCodeEncoder encoder = new MessagingToolkit.QRCode.Codec.QRCodeEncoder();
+                    encoder.QRCodeScale = 24;
+                    Bitmap qrBMP = encoder.Encode(ticketNo);
+
+                    graphicsImage.DrawImage(qrBMP, 1663, 300, 300, 300);
+
+                    if (Convert.ToInt32(ticketNo) >= 1955 && Convert.ToInt32(ticketNo) <= 2044)
+                    {
+                    }
+                    else {
+                        //Set the alignment based on the coordinates   
+                        StringFormat stringformat = new StringFormat();
+                        stringformat.Alignment = StringAlignment.Far;
+                        stringformat.LineAlignment = StringAlignment.Far;
+                        Color StringColor = System.Drawing.ColorTranslator.FromHtml("#000");//direct color adding
+
+                        int x = 0;
+                        switch (staffNo.Length)
+                        {
+                            case 1: x = 1763; break;
+                            case 2: x = 1785; break; //+12
+                            case 3: x = 1806; break; //+21  +9
+                            case 4: x = 1832; break; //35 + 9
+                            case 5: x = 1850; break; //35 + 9
+                        }
+
+                        graphicsImage.DrawString(staffNo, new Font("arial", 10,
+                        FontStyle.Regular), new SolidBrush(StringColor), new Point(x, 680),
+                        stringformat);
+
+                        x = 1773;
+                        switch (groupName.Length)
+                        {
+                            case 7: x = 1862; break;
+                            case 10: x = 1883; break;
+                            case 12: x = 1937; break;
+                            case 16: x = 1982; break;
+                            case 17: x = 2042; break;
+                        }
+
+                        graphicsImage.DrawString(groupName, new Font("arial", 10,
+                        FontStyle.Regular), new SolidBrush(StringColor), new Point(x, 760),
+                        stringformat);
+
+                        switch (tableNo.Length)
+                        {
+                            case 1: x = 1767; break;
+                            case 2: x = 1788; break; //+12
+                            case 3: x = 1813; break; //+21  +9
+                            case 4: x = 1834; break; //35 + 9
+                            case 5: x = 1861; break; //35 + 9
+                        }
+                        graphicsImage.DrawString(tableNo, new Font("arial", 10,
+                        FontStyle.Regular), new SolidBrush(StringColor), new Point(x, 841),
+                        stringformat);
+
+
+                        switch (ticketNo.Length)
+                        {
+                            case 1: x = 1780; break;
+                            case 2: x = 1802; break; //+12
+                            case 3: x = 1824; break; //+21  +9
+                            case 4: x = 1850; break; //35 + 9
+                            case 5: x = 1870; break; //35 + 9
+                        }
+                        graphicsImage.DrawString(ticketNo, new Font("arial", 10,
+                        FontStyle.Regular), new SolidBrush(StringColor), new Point(x, 921),
+                        stringformat);
+                    }
+                    bitmap.Save(AppDomain.CurrentDomain.BaseDirectory + @"ticketImg/" + staffNo + ".png");
+                    //bitmap.Save(Response.OutputStream, ImageFormat.Png);
                 }
-
-                graphicsImage.DrawString(staffNo, new Font("arial", 10,
-                FontStyle.Regular), new SolidBrush(StringColor), new Point(x, 740),
-                stringformat);
-
-                x = 1729;
-                int add = 12;
-                for (int i = 0; i < groupName.Length - 1; i++)
+                catch (Exception ex)
                 {
-                    x += add;
-                    add += 1;
+                    Console.WriteLine("StaffNo. " + staffNo + " " + ex.Message);
                 }
-
-                graphicsImage.DrawString(groupName, new Font("arial", 10,
-                FontStyle.Regular), new SolidBrush(StringColor), new Point(x, 820),
-                stringformat);
-
-                switch (tableNo.Length)
-                {
-                    case 1: x = 1730; break;
-                    case 2: x = 1785; break; //+12
-                    case 3: x = 1806; break; //+21  +9
-                    case 4: x = 1836; break; //35 + 9
-                }
-                graphicsImage.DrawString(tableNo, new Font("arial", 10,
-                FontStyle.Regular), new SolidBrush(StringColor), new Point(x, 901),
-                stringformat);
-
-
-                switch (ticketNo.Length)
-                {
-                    case 1: x = 1763; break;
-                    case 2: x = 1785; break; //+12
-                    case 3: x = 1806; break; //+21  +9
-                    case 4: x = 1836; break; //35 + 9
-                }
-                graphicsImage.DrawString(ticketNo, new Font("arial", 10,
-                FontStyle.Regular), new SolidBrush(StringColor), new Point(x, 981),
-                stringformat);
-
-                bitmap.Save(AppDomain.CurrentDomain.BaseDirectory  + @"ticketImg/" + staffNo + ".png");
-                //bitmap.Save(Response.OutputStream, ImageFormat.Png);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("StaffNo. " + staffNo + " " + ex.Message); 
-            }
+    
 
             return staffNo + ".png";
         
@@ -161,9 +173,9 @@ namespace qr_generator
         public static void ConvertToPdf()
         {
             Console.WriteLine("Converting to pdf...");
-            using (MagickImageCollection collection = new MagickImageCollection())
+            for (int i = 0; i < imageName.Count; i++)
             {
-                for (int i = 0; i < imageName.Count; i++)
+                using (MagickImageCollection collection = new MagickImageCollection())
                 {
                     collection.Add(new MagickImage(AppDomain.CurrentDomain.BaseDirectory + @"ticketImg/" + imageName[i]));
                     // Create pdf file with two pages
