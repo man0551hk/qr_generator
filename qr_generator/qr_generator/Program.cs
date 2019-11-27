d2e3058087e58ade546b0b082df7c25d33d4165e
Jerry Wong <man0551hk@yahoo.com.hk>
2019-11-27 21:18:55 +0800

    update

--- qr_generator/qr_generator/Program.cs
+++ qr_generator/qr_generator/Program.cs
@@ -22,6 +22,8 @@ namespace qr_generator
         public static PrivateFontCollection pfc = new PrivateFontCollection();
         public static List<string> strList = new List<string>();
         public static List<string> strList2 = new List<string>();
+        public static List<string> strList3 = new List<string>();
+        public static List<string> strList4 = new List<string>();
         static void Main(string[] args)
         {
             pfc.AddFontFile(AppDomain.CurrentDomain.BaseDirectory + "Palatino.ttf");
@@ -35,13 +37,15 @@ namespace qr_generator
             }
             AddList();
             AddList2();
+            AddList3();
+            AddList4();
             ReadExcel();
             Console.WriteLine("End");
             Console.WriteLine("Press enter to exit");            
         }
 
         //DrawImage(title, lastName, firstName, perferredName, empNumber);
-        public static string DrawImage(string title, string lastName, string firstName, string perferredName, string empNumber)
+        public static string DrawImage(string title, string lastName, string firstName, string perferredName, string empNumber, int qrCode)
         {
             Console.WriteLine("Generating images...");
 
@@ -63,7 +67,7 @@ namespace qr_generator
 
                 MessagingToolkit.QRCode.Codec.QRCodeEncoder encoder = new MessagingToolkit.QRCode.Codec.QRCodeEncoder();
                 encoder.QRCodeScale = 24;
-                Bitmap qrBMP = encoder.Encode(empNumber);
+                Bitmap qrBMP = encoder.Encode(qrCode.ToString());
                 graphicsImage.DrawImage(qrBMP, 1453, 245, 220, 220);
                 Color StringColor = System.Drawing.ColorTranslator.FromHtml("#fff");
 
@@ -74,22 +78,43 @@ namespace qr_generator
                     if (str == empNumber) 
                     {
                         fontSize = 45;
-                        break;
                     }
                 }
-                foreach (string str in strList2)
+                foreach (string str2 in strList2)
                 {
-                    if (str == empNumber)
+                    if (str2 == empNumber)
+                    {
+                        fontSize = 40;
+                    }
+                }
+                foreach (string str3 in strList3)
+                {
+                    if (str3 == empNumber)
                     {
                         fontSize = 35;
-                        break;
                     }
                 }
+
+                foreach (string str4 in strList4)
+                {
+                    if (str4 == empNumber)
+                    {
+                        fontSize = 30;
+                    }
+                }
+
+                if (empNumber == "52956")
+                {
+                    fontSize = 25;
+                }
+
                 string firstLine = title + " " + lastName + " " + firstName;
                 Font font = new Font(pfc.Families[0], fontSize, FontStyle.Regular, GraphicsUnit.Pixel);
                 int halfWidth = TextRenderer.MeasureText(firstLine, font).Width / 2;
                 graphicsImage.DrawString(firstLine, font, new SolidBrush(StringColor), new Point(middle - halfWidth + 17, 487));
 
+                fontSize = 55;
+                font = new Font(pfc.Families[0], fontSize, FontStyle.Regular, GraphicsUnit.Pixel);
                 halfWidth = TextRenderer.MeasureText(perferredName, font).Width /2 ;
                 graphicsImage.DrawString(perferredName, font, new SolidBrush(StringColor), new Point(middle - halfWidth + 17, 547));
 
@@ -97,7 +122,8 @@ namespace qr_generator
                 halfWidth = TextRenderer.MeasureText(empNumber, font).Width / 2;
                 graphicsImage.DrawString(empNumber, font, new SolidBrush(StringColor), new Point(middle - halfWidth + 5 ,720));
 
-                bitmap.Save(AppDomain.CurrentDomain.BaseDirectory + @"ticketImg/ticket_" + empNumber + ".jpg");
+                bitmap.Save(AppDomain.CurrentDomain.BaseDirectory + @"ticketImg/ticket_" + qrCode + "_" + empNumber + ".jpg");
+               
             }
             catch (Exception ex)
             {
@@ -119,7 +145,7 @@ namespace qr_generator
 
             foreach (FileInfo file in Files)
             {
-                Console.WriteLine(file.FullName);
+                //Console.WriteLine(file.FullName);
                 using (MagickImageCollection collection = new MagickImageCollection())
                 {
                     collection.Add(new MagickImage(file.FullName));
@@ -150,51 +176,61 @@ namespace qr_generator
 
             ISheet sheet = hssfwb.GetSheetAt(0);
 
+            int qrCode = 11546;
 
-            for (int row = 1; row <= sheet.LastRowNum; row++) // start from row 4
+            qrCode = 12507;
+            for (int row = 1; row <= 30; row++) // start from row 4
             {
-                if (sheet.GetRow(row) != null) //null is when the row only contains empty cells 
-                {
-                    IRow irow = sheet.GetRow(row);
-     
-                    ICell titleCell = irow.GetCell(0);
-                    ICell lastNameCell = irow.GetCell(1);
-                    ICell firstNameCell = irow.GetCell(2);
-                    ICell perferredNameCell = irow.GetCell(3);
-                    ICell empNumberCell = irow.GetCell(4);
-
-                    if (titleCell != null && lastNameCell != null && firstNameCell != null && perferredNameCell != null && empNumberCell != null)
-                    {
-                        string title = titleCell.StringCellValue;
-                        string lastName = lastNameCell.StringCellValue;
-                        string firstName = firstNameCell.StringCellValue;
-                        string perferredName = perferredNameCell.StringCellValue;
-                        string empNumber = "";
-                        try
-                        {
-                            empNumber = empNumberCell.StringCellValue;
-                        }
-                        catch (Exception ex)
-                        {
-                            empNumber = Convert.ToInt32(empNumberCell.NumericCellValue).ToString();
-                        }
-                        if (empNumber == "")
-                        {
-                            File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "log.txt", "Row " + row.ToString() + System.Environment.NewLine);
-                        }
-                        else 
-                        {
-                            DrawImage(title, lastName, firstName, perferredName, empNumber);
-                        }
-                        Console.WriteLine(title + " " + lastName + " " + firstName + " " + perferredName + " " + empNumber);
-                        //
-                    }
-                    else {
-                        File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "log.txt", "Row " + row.ToString() + System.Environment.NewLine);
-                    }
-                    //break;
-                }
+                DrawImage("", "", "", "", "", qrCode);
+                qrCode++;
             }
+
+            //for (int row = 1; row <= sheet.LastRowNum; row++) // start from row 4
+            //{
+            //    if (sheet.GetRow(row) != null) //null is when the row only contains empty cells 
+            //    {
+            //        IRow irow = sheet.GetRow(row);
+
+            //        ICell titleCell = irow.GetCell(2);
+            //        ICell lastNameCell = irow.GetCell(5);
+            //        ICell firstNameCell = irow.GetCell(3);
+            //        ICell perferredNameCell = irow.GetCell(4);
+            //        ICell empNumberCell = irow.GetCell(1);
+
+            //        if (titleCell != null && lastNameCell != null && firstNameCell != null && perferredNameCell != null && empNumberCell != null)
+            //        {
+
+            //            string title = titleCell.StringCellValue;
+            //            string lastName = lastNameCell.StringCellValue;
+            //            string firstName = firstNameCell.StringCellValue;
+            //            string perferredName = perferredNameCell.StringCellValue;
+            //            string empNumber = "";
+            //            try
+            //            {
+            //                empNumber = empNumberCell.StringCellValue;
+            //            }
+            //            catch (Exception ex)
+            //            {
+            //                empNumber = Convert.ToInt32(empNumberCell.NumericCellValue).ToString();
+            //            }
+            //            if (empNumber == "")
+            //            {
+            //                File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "log.txt", "Row " + row.ToString() + System.Environment.NewLine);
+            //            }
+            //            else
+            //            {
+            //                DrawImage(title, lastName, firstName, perferredName, empNumber, qrCode);
+            //            }
+
+            //            qrCode++;
+            //        }
+            //        else
+            //        {
+            //            File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "log.txt", "Row " + row.ToString() + System.Environment.NewLine);
+            //        }
+            //        break;
+            //    }
+            //}
         }
     
         public static void AddList()
@@ -243,27 +279,58 @@ namespace qr_generator
 
         public static void AddList2()
         {
-            strList.Add("45308");
-            strList.Add("45658");
-            strList.Add("51615");
-            strList.Add("52521");
-            strList.Add("52956");
-            strList.Add("53643");
-            strList.Add("54021");
-            strList.Add("58453");
-            strList.Add("59089");
-            strList.Add("60336");
-            strList.Add("60883");
-            strList.Add("60971");
-            strList.Add("68336");
-            strList.Add("70281");
-            strList.Add("71770");
-            strList.Add("72681");
-            strList.Add("72824");
-            strList.Add("73649");
-            strList.Add("74065");
-            strList.Add("75792");
-            strList.Add("76491");
+            strList2.Add("45308");
+            strList2.Add("45658");
+            strList2.Add("51615");
+            strList2.Add("52521");
+            strList2.Add("52956");
+            strList2.Add("53643");
+            strList2.Add("54021");
+            strList2.Add("58453");
+            strList2.Add("59089");
+            strList2.Add("60336");
+            strList2.Add("60883");
+            strList2.Add("60971");
+            strList2.Add("68336");
+            strList2.Add("70281");
+            strList2.Add("71770");
+            strList2.Add("72681");
+            strList2.Add("72824");
+            strList2.Add("73649");
+            strList2.Add("74065");
+            strList2.Add("75792");
+            strList2.Add("76491");
+        }
+
+        public static void AddList3()
+        {
+            strList3.Add("52521");
+            strList3.Add("52956");
+
+            strList3.Add("54021");
+            strList3.Add("58453");
+            strList3.Add("59089");
+
+            strList3.Add("60883");
+
+            strList3.Add("68336");
+
+            strList3.Add("71770");
+            strList3.Add("72681");
+            strList3.Add("72824");
+            strList3.Add("75792");
+    
+        }
+
+        public static void AddList4()
+        {
+ 
+            strList4.Add("52956");
+
+            strList4.Add("68336");
+
+            strList4.Add("72824");
+
         }
     }
 }
